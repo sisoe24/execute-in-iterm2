fe0f45b5e51a94736fd461a44cd79084244e9129
virgilsisoe <virgilsisoe@gmail.com>
2022-03-22 21:41:37 -0400

    show button only for supported files

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,29 @@
 import * as vscode from "vscode";
 import { executeFileCommand, executeInputCommand, resetTabsId } from "./send_to_iterm";
 
+import * as utils from "./utils";
+
+/**
+ * Register the extensions of the files commands.
+ * 
+ * This will create a list of extensions which can be used in a `when-clause-context`
+ * for enabling/disabling the button in the editor toolbar.
+ * 
+ * Basically: If `resourceExtname` is in the list of the file commands extensions,
+ * then show the button, otherwise do not.
+ */
+function registerExtensions() {
+    const fileExtensions = [];
+    for (const fileExt of Object.keys(utils.extensionConfig("fileCommands") as {})) {
+        fileExtensions.push(`.${fileExt}`);
+    }
+    // https://code.visualstudio.com/api/references/when-clause-contexts#in-conditional-operator
+    vscode.commands.executeCommand("setContext", "ext.supportedFiles", fileExtensions);
+}
+
 export function activate(context: vscode.ExtensionContext): void {
     resetTabsId();
+    registerExtensions();
 
     context.subscriptions.push(
         vscode.commands.registerCommand("execute-in-iterm2.executeFileCommand", () => {
